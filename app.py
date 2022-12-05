446881707fd28863e3a2e6d703f8a7ae1f7bf0f5
Sifat Ahmed <sifat.ahmed@eii-net.co.jp>
2022-12-05 15:10:33 +0900

    Created a web app with drawing ability

---
+++ app.py
@@ -0,0 +1,15 @@
+## first importing the necessary files
+from flask import Flask, render_template
+## creating an app reference for flask
+app = Flask(__name__)
+
+
+## now adding a route to the application
+@app.route('/')
+def index_page():
+    return render_template('index.html', my_name='Testing')
+
+
+if __name__ == '__main__':
+    app.run(port=8000)
+
