e89c01f8e603b1897b28d953fb44115aa1a8d273
Sifat Ahmed <sifat.ahmed@eii-net.co.jp>
2023-01-24 17:27:04 +0900

    added fabric

--- static/scripts/engine.js
+++ static/scripts/engine.js
@@ -1,132 +1,69 @@
-var width = window.innerWidth;
-var height = window.innerHeight;
+var data;
+var image_height, image_width;
+var imgLoader = new Image();
+var canvas = new fabric.Canvas('mainView', {height: 600, width:800});
 
-//var img = document.getElementById('baseImg');
-//console.log(img.src);
-
-var stage = new Konva.Stage({
-    container: 'container',
-    width: 800,
-    height: 600,
-});
-
-var backGround = new Konva.Layer();
-var foreGround = new Konva.Layer();
-stage.add(backGround);
-stage.add(foreGround);
-// main API:
-var yoda;
-var imageObj = new Image();
-var group = new Konva.Group();
-
-var lines=[], single_line=[];
-var all_points = [];
-
-
-
-imageObj.onload = function() {
-    yoda = new Konva.Image({
-    x: 0,
-    y: 0,
-    image: imageObj,
-    width: 800,
-    height: 600,
-});
-
-// add the shape to the layer
-backGround.add(yoda);
-};
-
-imageObj.src = mysrc;
-
-var prev = 0;
-var points = [];
-
-function writeMessage(message) {
-    text.text('Message ' + message);
-}
-var text = new Konva.Text({
-    x: 10,
-    y: 10,
-    fontFamily: 'Calibri',
-    fontSize: 24,
-    text: '',
-    fill: 'red',
-});
-
-
-
-// dragging a circle
-
-//for (var i = 0; i < points.length; i++){
-//    console.log(i);
-//
-//    points[i].on('dragmove', function(){
-//        writeMessage('2. Point '+ i + ' dragging!');
-//        console.log('2. Point '+ i + ' dragging!');
-//    });
-//
+//function getImageWidth(imgSrc, callback) {
+//    imgLoader.onload = function() { // assign onload handler
+//        callback(this.width);
+//    }
+//    imgLoader.src = imgSrc; // set the image source
 //}
+//function getImageHeight(imgSrc, callback) {
+//    imgLoader.onload = function() { // assign onload handler
+//        callback(this.height);
+//    }
+//    imgLoader.src = imgSrc; // set the image source
+//}
+////canvas.onload = function(){
+////var canvas = new fabric.Canvas('mainView');
+//getImageWidth(mysrc, function(width){
+//    image_width = width;
+//});
+//
+//getImageHeight(mysrc, function(height){
+//    image_height = height;
+//});
+//
+function getImageSize(imgSrc) {
+    imgLoader.onload = function() { // assign onload handler
+        var height = imgLoader.height;
+        var width = imgLoader.width;
 
-stage.on('click', function (e) {
+        canvas.setHeight(height);
+        canvas.setWidth(width);
 
-    var x = stage.getPointerPosition().x;
-    var y = stage.getPointerPosition().y;
+        var image = new fabric.Image(imgLoader);
+        image.set({
+            angle: 0,
+            padding: 10,
+            cornersize:10,
 
-    var circle = new Konva.Circle({
-        x: x,
-        y: y,
-        radius: 4,
-        fill: 'red',
-        stroke: 'black',
-        strokeWidth: 1,
-        draggable: true
-    });
-    points.push(circle);
-    foreGround.add(circle);
+      });
+      //canvas.centerObject(image);
+      canvas.add(image);
 
-    for (var i = 0; i < points.length; i+=2){
-    //console.log('Point ' + [i] + ' ' + points[i].x() + ' ' + points[i].y());
-
-    if (i+1 < points.length) {
-        var line = new Konva.Line({
-            points: [points[i].x(), points[i].y(), points[i+1].x(), points[i+1].y()],
-            stroke: 'green',
-            strokeWidth: 2,
-            lineJoin: 'round',
-
-        });
-        foreGround.add(line);
-        lines.push(line);
-        }
     }
 
-//    circle.on('dragmove', function(){
-//        writeMessage('Point '+ i + ' dragging!');
-//        console.log('Point '+ i + ' dragging!');
-//    });
-
-
-
-});
-
-
-for (var j = 0; j < points.length; j++){
-
-    points[j].on('dragmove', function(){
-        console.log('2. Point '+ j + ' ' + points[1].x());
-        writeMessage('2. Point '+ j + ' dragging!');
-        //console.log('Lines', lines.length);
-
-    });
-
+    imgLoader.src = imgSrc; // set the image source
 }
 
-var tr = new Konva.Transformer();
+getImageSize(mysrc);
 
-foreGround.add(text);
-foreGround.add(tr);
-tr.nodes(points);
+canvas.on('mouse:wheel', function(opt) {
+  var delta = opt.e.deltaY;
+  var zoom = canvas.getZoom();
+  zoom *= 0.999 ** delta;
+  if (zoom > 20) zoom = 20;
+  if (zoom < 0.01) zoom = 0.01;
+  canvas.setZoom(zoom);
+  opt.e.preventDefault();
+  opt.e.stopPropagation();
+})
 
-//yoda.moveDown();
-backGround.zIndex(0);
+canvas.renderAll();
+
+
+
+
+// Callback: https://stackoverflow.com/questions/12344586/how-to-return-multiple-variables-in-javascript-callback-function
